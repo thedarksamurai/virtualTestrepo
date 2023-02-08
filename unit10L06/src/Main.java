8515130b0de5d80aa55b203916b08efcf2604ab6
thedarksamurai <halokiller36@gmail.com>
2023-02-08 14:11:45 -0700

    java date and sql date

---
+++ unit10L06/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println(now);
+
+        java.sql.Date date = new java.sql.Date(476789);
+        System.out.println(date);
+    }
+}
